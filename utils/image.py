72986e6ee4fc3f51d2150ee17d9176615342d19d
UtopicPanther <65122621+UtopicPanther@users.noreply.github.com>
2020-05-25 23:07:20 +0800

    add very basic youtube video support

--- utils/image.py
+++ utils/image.py
@@ -17,4 +17,7 @@ def dom(rr, image, game_name):
         if "sensitive" in image and image["sensitive"] == True:
             return '<img class="sensitive_img hide" data-realsrc="%s" src="data:image/png;base64,">' % uri(rr, image["uri"], game_name)
 
+        if "type" in image and image["type"] == "youtube":
+            return '<iframe width="100%%" height="342" src="https://www.youtube.com/embed/%s" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' % image["uri"].split(":")[1]
+
         return '<img src="%s">' % uri(rr, image, game_name)
