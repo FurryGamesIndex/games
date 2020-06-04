557c58d6312466bdc501890cc6551f22d06be3c2
UtopicPanther <65122621+UtopicPanther@users.noreply.github.com>
2020-06-04 23:18:00 +0800

    ci: use GITHUB_EVENT_PATH

--- scripts/newgame-tg.sh
+++ scripts/newgame-tg.sh
@@ -2,9 +2,15 @@
 
 nl=$'\n'
 
-[ -z "$BASE_REF" ] && BASE_REF="${SHA}~1"
+BASE_REF=$(jq --raw-output .commits[0].id "$GITHUB_EVENT_PATH")
+END_REF=$(jq --raw-output .commits[-1].id "$GITHUB_EVENT_PATH")
 
-games=$(git diff --name-only --diff-filter=A "${BASE_REF}" "${SHA}" | grep '^games/' | sed 's|games/\([^.]*\).yaml|<a href="furrygamesindex.github.io/zh-cn/games/\1.html">\1</a>|g')
+echo "BASE_REF: $BASE_REF"
+echo "END_REF: $END_REF"
+
+[ "$BASE_REF" = "$END_REF" ] && BASE_REF="${SHA}~1"
+
+games=$(git diff --name-only --diff-filter=A "${BASE_REF}" "${END_REF}" | grep '^games/' | sed 's|games/\([^.]*\).yaml|<a href="furrygamesindex.github.io/zh-cn/games/\1.html">\1</a>|g')
 if [ -n "$games" ]; then
 	curl "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" --data-urlencode "chat_id=${TELEGRAM_TO}" --data-urlencode "disable_web_page_preview=true" --data-urlencode "parse_mode=html" --data-urlencode "text=[Github] 已添加游戏：${nl}${games}" > /dev/null
 else
