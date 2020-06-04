f52984493824545f71f6abed951fa16c7e104c96
UtopicPanther <65122621+UtopicPanther@users.noreply.github.com>
2020-06-04 22:51:40 +0800

    add new games telegram notify

---
+++ scripts/newgame-tg.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+nl=$'\n'
+games=$(git diff --name-only --diff-filter=A "${BASE_REF}" "${SHA}" | grep '^games/' | sed 's|games/\([^.]*\).yaml|<a href="furrygamesindex.github.io/zh-cn/games/\1.html">\1</a>|g')
+if [ -n "$games" ]; then
+	curl "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" --data-urlencode "chat_id=${TELEGRAM_TO}" --data-urlencode "disable_web_page_preview=true" --data-urlencode "parse_mode=html" --data-urlencode "text=[Github] 已添加游戏：${nl}${games}" > /dev/null
+else
+	echo "No games added"
+fi
+
+exit 0
