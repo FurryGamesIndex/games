ad93079a4ba1f12ad670dcab96dc2501b7deb5a0
UtopicPanther <65122621+UtopicPanther@users.noreply.github.com>
2020-06-04 22:59:07 +0800

    ci: try fix BASE_REF

--- scripts/newgame-tg.sh
+++ scripts/newgame-tg.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 nl=$'\n'
+
+[ -z "$BASE_REF" ] && BASE_REF="${SHA}~1"
+
 games=$(git diff --name-only --diff-filter=A "${BASE_REF}" "${SHA}" | grep '^games/' | sed 's|games/\([^.]*\).yaml|<a href="furrygamesindex.github.io/zh-cn/games/\1.html">\1</a>|g')
 if [ -n "$games" ]; then
 	curl "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" --data-urlencode "chat_id=${TELEGRAM_TO}" --data-urlencode "disable_web_page_preview=true" --data-urlencode "parse_mode=html" --data-urlencode "text=[Github] 已添加游戏：${nl}${games}" > /dev/null
