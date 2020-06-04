c414162e2cbe25d8527d6740931ee48363ba5af8
UtopicPanther <65122621+UtopicPanther@users.noreply.github.com>
2020-06-05 00:05:07 +0800

    scripts/newgame-tg.sh: should ignore l10n yaml files

--- scripts/newgame-tg.sh
+++ scripts/newgame-tg.sh
@@ -11,9 +11,12 @@ echo "END_REF: $END_REF"
 #[ "$BASE_REF" = "$END_REF" ] && BASE_REF="${END_REF}~1"
 BASE_REF="${BASE_REF}~1"
 
-games=$(git diff --name-only --diff-filter=A "${BASE_REF}" "${END_REF}" | grep '^games/' | sed 's|games/\([^.]*\).yaml|<a href="furrygamesindex.github.io/zh-cn/games/\1.html">\1</a>|g')
+games=$(git diff --name-only --diff-filter=A "${BASE_REF}" "${END_REF}" | grep '^games/[^/]*.yaml' | sed 's|games/\([^.]*\).yaml|<a href="furrygamesindex.github.io/zh-cn/games/\1.html">\1</a>|g')
+
+echo "$games"
+
 if [ -n "$games" ]; then
-	curl "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" --data-urlencode "chat_id=${TELEGRAM_TO}" --data-urlencode "disable_web_page_preview=true" --data-urlencode "parse_mode=html" --data-urlencode "text=[Github] 已添加游戏：${nl}${games}${nl}${nl}需要等待 1 分钟或更长的时间后才能访问"
+	curl "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" --data-urlencode "chat_id=${TELEGRAM_TO}" --data-urlencode "disable_web_page_preview=true" --data-urlencode "parse_mode=html" --data-urlencode "text=[Github] 已添加游戏：${nl}${games}${nl}${nl}需要等待 1 分钟或更长的时间后才能访问" > /dev/null
 else
 	echo "No games added"
 fi
