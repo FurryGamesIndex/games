4743153270e573fc1f9dd8f8d49247bbca089d6f
UtopicPanther <65122621+UtopicPanther@users.noreply.github.com>
2020-06-08 17:43:39 +0800

    zhconv.py: add option to disable builtin path and add custom path

--- zhconv.py
+++ zhconv.py
@@ -4,6 +4,16 @@
 import os
 from opencc import OpenCC
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--opencc-profile', type=str, help='Use specify OpenCC profile(not impl)')
+parser.add_argument('--no-builtin', default=False, action='store_true', help='Do not run builtin path conv')
+parser.add_argument('--builtin-reverse', default=False, action='store_true', help='zh-tw to zh-cn for builtin dir conv(not impl)')
+parser.add_argument('extra_pathes', type=str, default=[], help='add extra path', nargs="*")
+
+args = parser.parse_args()
+
 root_dir = "."
 games_dir = os.path.join("games", "l10n")
 uil10n_dir = "uil10n"
@@ -33,9 +43,20 @@ def convdir(od, td):
 
 	    conv(file, os.path.join(td, f))
 
-convdir(os.path.join(games_dir, origin), os.path.join(games_dir, to))
-conv(os.path.join(uil10n_dir, origin + ".yaml"), os.path.join(uil10n_dir, to + ".yaml"), True)
-conv(os.path.join(root_dir, "README.%s.md" % origin), os.path.join(root_dir, "README.%s.md" % to), True)
-conv(os.path.join(doc_dir, "Contribute.%s.md" % origin), os.path.join(doc_dir, "Contribute.%s.md" % to), True)
-conv(os.path.join(doc_dir, "tags.%s.md" % origin), os.path.join(doc_dir, "tags.%s.md" % to), True)
-conv(os.path.join(doc_dir, "faq.%s.md" % origin), os.path.join(doc_dir, "faq.%s.md" % to), True)
+if not args.no_builtin:
+    convdir(os.path.join(games_dir, origin), os.path.join(games_dir, to))
+    conv(os.path.join(uil10n_dir, origin + ".yaml"), os.path.join(uil10n_dir, to + ".yaml"), True)
+    conv(os.path.join(root_dir, "README.%s.md" % origin), os.path.join(root_dir, "README.%s.md" % to), True)
+    conv(os.path.join(doc_dir, "Contribute.%s.md" % origin), os.path.join(doc_dir, "Contribute.%s.md" % to), True)
+    conv(os.path.join(doc_dir, "tags.%s.md" % origin), os.path.join(doc_dir, "tags.%s.md" % to), True)
+    conv(os.path.join(doc_dir, "faq.%s.md" % origin), os.path.join(doc_dir, "faq.%s.md" % to), True)
+
+for i in args.extra_pathes:
+    f = i.split(":")[0]
+    t = i.split(":")[1]
+    if os.path.isfile(f):
+        conv(f, t)
+    elif os.path.isdir(f):
+        convdir(f, t, True)
+    else:
+        print("Warning: unknown path: %s" % f)
