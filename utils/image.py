759fa12bc249f9aca6b88cd6eb95f64e8b5248e8
UtopicPanther <65122621+UtopicPanther@users.noreply.github.com>
2020-06-26 20:55:08 +0800

    templates/list,game: SEO: add alt attribute to images

--- utils/image.py
+++ utils/image.py
@@ -3,17 +3,18 @@
 import re
 import os
 import hashlib
+from html import escape
 from utils.webutils import dl
 from __main__ import args
 
 regexp = re.compile("^[a-zA-Z0-9\-]+:/{0,2}[^/]+")
 
-def uri(rr, image, game_name):
+def uri(rr, image, gameid):
     if (regexp.match(image)):
         if args.download_external_images:
             uri = image
             sum = hashlib.sha1(image.encode("utf-8")).hexdigest();
-            image = "assets/" + game_name + "/" + sum + os.path.splitext(uri)[1]
+            image = "assets/" + gameid + "/" + sum + os.path.splitext(uri)[1]
 
             if not os.path.isfile(os.path.join(args.output, image)):
                 print("downloading %s %s" % (sum, uri))
@@ -23,24 +24,24 @@ def uri(rr, image, game_name):
 
         return image
     else:
-        return rr + "/assets/" + game_name + "/" + image
+        return rr + "/assets/" + gameid + "/" + image
 
-def _media_image(rr, image, game_name):
+def _media_image(rr, image, gameid, name):
     if "sensitive" in image and image["sensitive"] == True:
-        return '<img class="sensitive_img hide" data-realsrc="%s" src="data:image/png;base64,">' % uri(rr, image["uri"], game_name)
+        return '<img class="sensitive_img hide" data-realsrc="%s" src="data:image/png;base64,">' % uri(rr, image["uri"], gameid)
     else:
-        return '<img src="%s">' % uri(rr, image["uri"], game_name)
+        return '<img alt="%s" src="%s">' % (escape(name), uri(rr, image["uri"], gameid))
 
-def _media_youtube(rr, image, game_name):
+def _media_youtube(rr, image, gameid, name):
     return '<iframe width="100%%" height="342" src="https://www.youtube.com/embed/%s" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' % image["uri"].split(":")[1]
 
-def _media_video(rr, image, game_name):
+def _media_video(rr, image, gameid, name):
     elm = '<video controls width="100%">'
     for i in image["src"]:
         elm += '<source src="%s" type="%s">' % (i["uri"], i["mime"])
     return elm + "</video>"
 
-def dom(rr, image, game_name):
+def dom(rr, image, gameid, name = ""):
     mode = _media_image
     image_meta = image
 
@@ -53,4 +54,4 @@ def dom(rr, image, game_name):
         if image["type"] == "video":
             mode = _media_video
 
-    return mode(rr, image_meta, game_name)
+    return mode(rr, image_meta, gameid, name)
