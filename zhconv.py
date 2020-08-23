c60828cb2cef33f1e03e9c605b30fa3f2e159b4b
UtopicPanther <65122621+UtopicPanther@users.noreply.github.com>
2020-08-23 13:13:13 +0800

    zhconv: X-Chinese-Converter-Hint supporting

--- zhconv.py
+++ zhconv.py
@@ -19,14 +19,12 @@
 # 
 
 import os
+import argparse
+import yaml
 from opencc import OpenCC
 
-import argparse
-
 parser = argparse.ArgumentParser()
-parser.add_argument('--opencc-profile', type=str, help='Use specify OpenCC profile(not impl)')
 parser.add_argument('--no-builtin', default=False, action='store_true', help='Do not run builtin path conv')
-parser.add_argument('--builtin-reverse', default=False, action='store_true', help='zh-tw to zh-cn for builtin dir conv(not impl)')
 parser.add_argument('extra_pathes', type=str, default=[], help='add extra path', nargs="*")
 
 args = parser.parse_args()
@@ -40,10 +38,15 @@ converter = OpenCC('s2twp.json')
 origin = "zh-cn"
 to = "zh-tw"
 
-def conv(of, tf, fixlinks = False):
+convertert2s = OpenCC('tw2sp.json')
+
+def conv(of, tf, fixlinks = False, conv=converter):
     print("Converting %s, fixlinks=%r" % (of, fixlinks))
     with open(of) as stream:
-        data = converter.convert(stream.read())
+        data = conv.convert(stream.read())
+
+    if conv is not converter and fixlinks:
+        raise NotImplementedError("fix links is not implemented for non-default converter")
 
     if fixlinks:
         data = data.replace("." + origin + ".", "." + to + ".")
@@ -52,16 +55,43 @@ def conv(of, tf, fixlinks = False):
     with open(tf, 'w') as stream:
         stream.write(data)
 
-def convdir(od, td):
-	for f in os.listdir(od):
-	    file = os.path.join(od, f)
-	    if (not os.path.isfile(file)) or (f[0] == '.'):
-                continue
+def __convgamesl10n(cn, tw, f, fn):
+    cnf = os.path.join(cn, f)
+    twf = os.path.join(tw, f)
 
-	    conv(file, os.path.join(td, f))
+    with open(fn) as stream:
+        data = yaml.safe_load(stream)
+
+    if "X-Chinese-Converter-Hint" in data:
+        data = data["X-Chinese-Converter-Hint"]
+        if "prefer" in data:
+            if data["prefer"] == "TW":
+                conv(twf, cnf, conv=convertert2s)
+            elif data["prefer"] == "CN":
+                conv(cnf, twf)
+            else:
+                raise ValueError("unsupported cchint prefer %s" % prefer)
+    else:
+        conv(cnf, twf)
+
+def convgamesl10n(cn, tw):
+    cnfiles = [i for i in os.listdir(cn) if os.path.isfile(os.path.join(cn, i)) and i[0] != '.']
+    for f in cnfiles:
+        fn = os.path.join(cn, f)
+        __convgamesl10n(cn, tw, f, fn)
+
+    twfiles = [i for i in os.listdir(tw) \
+                if os.path.isfile(os.path.join(tw, i)) and \
+                i[0] != '.' and \
+                i not in cnfiles]
+    for f in twfiles:
+        print("New TW file: %s" % f)
+        fn = os.path.join(tw, f)
+        __convgamesl10n(cn, tw, f, fn)
+
 
 if not args.no_builtin:
-    convdir(os.path.join(games_dir, origin), os.path.join(games_dir, to))
+    convgamesl10n(os.path.join(games_dir, origin), os.path.join(games_dir, to))
     conv(os.path.join(uil10n_dir, origin + ".yaml"), os.path.join(uil10n_dir, to + ".yaml"), True)
     conv(os.path.join(root_dir, "README.%s.md" % origin), os.path.join(root_dir, "README.%s.md" % to), True)
     conv(os.path.join(doc_dir, "Contribute.%s.md" % origin), os.path.join(doc_dir, "Contribute.%s.md" % to), True)
