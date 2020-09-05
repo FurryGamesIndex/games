723cf08fd990077266858526b22184c2e30aa3bb
UtopicPanther <65122621+UtopicPanther@users.noreply.github.com>
2020-09-05 19:54:34 +0800

    renderers/list: sorted with underscore and case ignored

--- renderers/list.py
+++ renderers/list.py
@@ -35,7 +35,7 @@ context = {
 def render(games, env, language, language_ui, output):
     context["lang"] = language
     context["ui"] = language_ui
-    context["games"] = games
+    context["games"] = dict(sorted(games.items(), key=lambda t: t[0].replace("_", "").upper()))
     with openw_with_sm(output, os.path.join(language, "list.html"), priority="0.6") as f:
         f.write(env.get_template("header.html").render(context))
         f.write(env.get_template("list.html").render(context))
