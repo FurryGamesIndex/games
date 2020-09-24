882e492ff07d833eea3ffa5315e14787b82a2cca
UtopicPanther <65122621+UtopicPanther@users.noreply.github.com>
2020-09-24 14:25:41 +0800

    utils/webp: add can_convert function

--- utils/webp.py
+++ utils/webp.py
@@ -17,10 +17,17 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # 
 
+import os
 import subprocess
 
 def cwebp(ofn, tfn):
     cp = subprocess.run(["cwebp", ofn, "-o", tfn], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     if cp.returncode != 0:
         raise RuntimeError(f"cwebp {ofn} error, outputs:\n{cp.stdout.decode('utf-8')}")
+
+allowed_suffixes = set(['.png', '.jpg', '.jpeg', '.tiff', '.webp'])
+
+def can_convert(fn):
+    suffix = os.path.splitext(fn)[1]
+    return suffix in allowed_suffixes
 
