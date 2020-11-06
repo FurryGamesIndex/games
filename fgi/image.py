301a0771bfdd897f569312aff3b75706cba48b43
UtopicPanther <65122621+UtopicPanther@users.noreply.github.com>
2020-11-06 21:01:21 +0800

    fgi/image: fixup webp converting

--- fgi/image.py
+++ fgi/image.py
@@ -169,15 +169,15 @@ def uri_to_html_image(rr, imageuri, gameid, alt = None):
 
         new_path = img.path + ".webp"
 
-        if os.path.exists(path) \
-                and not os.path.exists(new_path):
-            try:
-                webp.cwebp(path, new_path)
-                os.remove(path)
-                img.uri += ".webp"
-                img.path = new_path
-            except:
-                print(f"[warning] {path} can not be converted to webp")
+        if os.path.exists(path):
+            if os.path.exists(new_path):
+                try:
+                    webp.cwebp(path, new_path)
+                    img.uri += ".webp"
+                    img.path = new_path
+                except:
+                    print(f"[warning] {path} can not be converted to webp")
+            os.remove(path)
 
     hi = HTMLImage.from_image(img)
     hi.alt = alt
