99fa7c85b25a3057caea3aec712205fcf8f152bd
UtopicPanther <65122621+UtopicPanther@users.noreply.github.com>
2021-01-14 03:24:41 +0800

    renderers/author: render author pages

---
+++ fgi/renderers/author.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+# 
+# Copyright (C) 2020 Utopic Panther
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# 
+
+import os
+import re
+from fgi.base import make_wrapper
+from fgi.renderer import Renderer
+from fgi.i18n import get, get_mtime
+from fgi.link import link_info
+from fgi.seo import keywords
+
+class RendererAuthor(Renderer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.basectx = {
+            "rr": "../..",
+            "image": self.fctx.mfac,
+            "get": get,
+            "link_info": make_wrapper(link_info, self.fctx),
+            "mfac": self.fctx.mfac,
+        }
+
+        self.authors = self.lctx["authors"]
+        self.context = self.new_context()
+
+        lang_without_region = self.language
+        if '-' in lang_without_region:
+            lang_without_region = lang_without_region.split('-')[0]
+
+        self.context["lang_without_region"] = lang_without_region
+
+    def new_author_context(self):
+        return self.context.copy()
+
+    def render_author(self, author):
+        print("  => %s" % author["name"])
+
+        context = self.new_author_context()
+        context["author"] = author
+
+        meta = dict()
+        meta["title"] = author["name"]
+        meta["image"] = self.fctx.mfac.uri_to_html_image(context["rr"], author["avatar"], "_avatar").src
+        context["meta"] = meta
+
+        return self.env.get_template("author.html").render(context)
+
+    def render(self):
+        os.mkdir(self.getpath("authors"))
+
+        for _, author in self.authors.items():
+            with self.sm_openw("authors", author["id"] + ".html", priority="0.7") as f:
+                f.write(self.render_author(author))
+
+impl = RendererAuthor
