f58ae7822ee32877862fa6568e9061fa2d31e31c
UtopicPanther <65122621+UtopicPanther@users.noreply.github.com>
2021-03-12 00:29:31 +0800

    scripts/fix-mtime: make script runable on macOS
    You need install GNU date.
    Using Homebrew, run `brew install coreutils`

--- scripts/fix-mtime.sh
+++ scripts/fix-mtime.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+DATE="gdate"
+[ "$(uname -s)" = "Linux" ] && DATE="date"
+
 git ls-tree -r --name-only HEAD | while read filename; do 
 	unixtime=$(git log -1 --format="%at" -- "${filename}")
 
 	# do not makes lastmod smaller than first lastmod in sitemap.xml, hacking!
 	test "$unixtime" -lt "1599510461" && unixtime=1599510461
 
-	touchtime=$(date -d @$unixtime +'%Y%m%d%H%M.%S')
+	touchtime=$($DATE -d @$unixtime +'%Y%m%d%H%M.%S')
 	touch -t ${touchtime} "${filename}"
 done
