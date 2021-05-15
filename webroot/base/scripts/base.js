4e15f9bf04916d2291d115b4ee225d93b40f2746
UtopicPanther <upanther@furrygames.top>
2021-05-15 15:16:12 +0800

    webroot: scripts/base: remove useless things

--- webroot/base/scripts/base.js
+++ webroot/base/scripts/base.js
@@ -31,43 +31,6 @@ window.make_picture = (data, _prefix, picture) => {
 if ('serviceWorker' in navigator)
 	navigator.serviceWorker.register('/sw.js');
 
-window.add_infobar = (id, content) => {
-	if (localStorage.getItem("current_hide_id") === id)
-		return;
-
-	const div = $('#infobar_rt ._content');
-	if (div != null) {
-		const element = $('#infobar_rt');
-		div.innerHTML = content;
-		element.classList.remove('hide');
-		$('#infobar_rt ._cbtn').onclick = ((f, a, b) => event => f(a, b))((id, element) => {
-			event.preventDefault();
-			element.classList.add('hide');
-			localStorage.setItem("current_hide_id", id);
-		}, id, element);
-	}
-}
-
-let stagingLangMap = {'ja': 'Japanese'}
-let langs = ""
-for (const lang in stagingLangMap) {
-	if (navigator.languages.indexOf(lang) >= 0) {
-		langs += stagingLangMap[lang] + ", ";
-	}
-
-	langs = langs.slice(0, -2);
-
-	if (langs.length > 0) {
-		add_infobar('want-translator',
-			"Based on the language of your browser, it seems you know " +
-			langs + ". Would you like to help us translate the " + langs +
-			" version of the site? The maintainer can be contacted via " +
-			"<a href='https://github.com/FurryGamesIndex/games'>Github</a>, " +
-			"<a href='mailto:webmaster@furrygames.top'>Email</a> " +
-			"or <a href='https://t.me/UtopicPanther'>Telegram</a>.");
-	}
-}
-
 } catch (e) {
 	console.log("FGI base.js load failed")
 }
