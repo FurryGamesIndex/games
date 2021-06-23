d1c825daebc8996d6ad1c8e20413fadf102b5e6b
UtopicPanther <upanther@furrygames.top>
2021-06-23 14:56:56 +0800

    treewide: run zhconv

--- zhconv.py
+++ zhconv.py
@@ -98,6 +98,7 @@ if not args.no_builtin:
     conv(os.path.join(root_dir, "README.%s.md" % origin), os.path.join(root_dir, "README.%s.md" % to), True)
 
     conv(os.path.join(doc_dir, "Get-Involved.%s.md" % origin), os.path.join(doc_dir, "Get-Involved.%s.md" % to), True)
+    conv(os.path.join(doc_dir, "FGI-members.%s.md" % origin), os.path.join(doc_dir, "FGI-members.%s.md" % to), True)
     conv(os.path.join(doc_dir, "Contribute.%s.md" % origin), os.path.join(doc_dir, "Contribute.%s.md" % to), True)
     conv(os.path.join(doc_dir, "contribute_guide", "patches-submitting.%s.md" % origin), os.path.join(doc_dir, "contribute_guide", "patches-submitting.%s.md" % to), True)
     conv(os.path.join(doc_dir, "contribute_guide", "zhconv.%s.md" % origin), os.path.join(doc_dir, "contribute_guide", "zhconv.%s.md" % to), False)
